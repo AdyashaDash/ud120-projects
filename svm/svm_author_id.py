36a2d32046df7f316c8a4f2bb2544ce180c50572
Adyasha Dash <adyasha.dash@wingtra.com>
2018-07-25 22:37:26 +0200

    add print statement for last set of validation

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,25 +19,26 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-features_train = features_train[:len(features_train)/100]
-labels_train = labels_train[:len(labels_train)/100]
-
 from sklearn import svm, metrics
+
 t0 = time()
 clf = svm.SVC(kernel='rbf', C=10000)
 clf.fit(features_train, labels_train)
-print("training time:", round(time()-t0, 3), "s")
+print "training time:", round(time()-t0, 3), "s"
 
 t0 = time()
 labels_pred = clf.predict(features_test)
-print("prediction time:", round(time()-t0, 3), "s")
+print "prediction time:", round(time()-t0, 3), "s"
 
 print(metrics.accuracy_score(labels_test, labels_pred))
+
+count_chris = 0
+for label in labels_pred:
+	if label == 1:
+		count_chris += 1
+print 'no. of emails from Chris: ', count_chris
 #########################################################
 
 
