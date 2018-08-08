ef3fec0e2768e2d2a7f67433e58273fddbd20e65
Adyasha Dash <adyasha.dash@wingtra.com>
2018-08-08 23:51:21 +0200

    cleanup

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -36,6 +36,20 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.savefig(name)
     plt.show()
 
+def min_max(key_to_be_compared, dict_of_data):
+    max_value = 0.0
+    min_value = 10000000000000.0
+    for key1 in dict_of_data:
+        for key2 in dict_of_data[key1]:
+            if (key2 == key_to_be_compared and dict_of_data[key1][key2] 
+                != "NaN" and dict_of_data[key1][key2] != 0):
+                if (dict_of_data[key1][key2] > max_value):
+                    max_value = dict_of_data[key1][key2]
+                elif (dict_of_data[key1][key2] < min_value):
+                    min_value = dict_of_data[key1][key2]
+
+    print "max value of", key_to_be_compared, "is:", max_value
+    print "min value of", key_to_be_compared, "is:", min_value
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
@@ -43,26 +57,16 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
 
-max_salary = 0.0
-min_salary = 10000000000000.0
-for key1 in data_dict:
-    for key2 in data_dict[key1]:
-        if (key2 == "salary" and data_dict[key1][key2] != "NaN" and data_dict[key1][key2] != 0):
-            if (data_dict[key1][key2] > max_salary):
-                max_salary = data_dict[key1][key2]
-            elif (data_dict[key1][key2] < min_salary):
-                min_salary = data_dict[key1][key2]
-
-print max_salary
-print min_salary
+min_max("exercised_stock_options", data_dict)
+min_max("salary", data_dict)
 
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
+# feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -71,7 +75,7 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
@@ -84,6 +88,6 @@ pred = kmeans.predict(finance_features)
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
-    Draw(pred, finance_features, poi, mark_poi=False, name="clusters-3.pdf", f1_name=feature_1, f2_name=feature_2)
+    Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
