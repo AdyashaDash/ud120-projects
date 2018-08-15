f427e8485fa996271f9ea77c7e5b4e77efeb429a
Adyasha Dash <adyasha.dash@wingtra.com>
2018-08-15 21:07:56 +0200

    stem and clean data

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -22,6 +22,9 @@ from parse_out_email_text import parseOutText
     The data is stored in lists and packed away in pickle files at the end.
 """
 
+def removeStopwords(text, stopwords):
+    pattern = re.compile(r"\b(" + "|".join(stopwords) + ")\\W", re.I) 
+    return pattern.sub("", text)
 
 from_sara  = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
@@ -48,18 +51,22 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            stemmed_email = parseOutText(email)
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            ### remove any instances of the stop words
+            stopwords =  ["sara", "shackleton", "chris", "germani"]
+            stemmed_cleaned_email = removeStopwords(stemmed_email, stopwords)
 
             ### append the text to word_data
+            word_data.append(stemmed_cleaned_email)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            from_data.append(int(name == "Chris"))
 
             email.close()
 
 print "emails processed"
+print word_data[152]
 from_sara.close()
 from_chris.close()
 
