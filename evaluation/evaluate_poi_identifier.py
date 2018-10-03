4d35274a50e25b02792e901e34e4c7be7172f36f
Adyasha Dash <adyasha.dash@wingtra.com>
2018-10-03 23:15:25 +0200

    wrap up

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,22 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import precision_score, recall_score
+
+X_train, X_test, y_train, y_test = train_test_split(
+    features, labels, test_size=0.30, random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+print clf.score(X_test, y_test)
+print len([y for y in y_test if y == 1]) #No. of actual POI
+print len(y_test) #No. of people in test dataset
+true_values = [i for i, j in zip(pred, y_test) if i == j] 
+print len([y for y in true_values if y == 1])#Find True positives
+print precision_score(y_test, pred)
+print recall_score(y_test, pred)
+
 
 
