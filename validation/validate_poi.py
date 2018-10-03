1f5bdbb0391900c0aed5eaf0fbcd839ee2ef6603
Adyasha Dash <adyasha.dash@wingtra.com>
2018-10-03 21:41:38 +0200

    from validation chapter

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -26,7 +26,13 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
-
 ### it's all yours from here forward!  
+from sklearn import tree
+from sklearn.model_selection import train_test_split
 
+X_train, X_test, y_train, y_test = train_test_split(
+    features, labels, test_size=0.30, random_state=42)
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(X_train, y_train)
+print clf.score(X_test, y_test)
 
